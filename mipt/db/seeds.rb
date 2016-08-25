40b7d7c39624ae6ef5eaa9429e3e16f6568fa1f5
Eric Booker <ericbooker12@gmail.com>
2016-08-24 17:24:16 -0700

    Create sample student roster and parsing seed file

--- mipt/db/seeds.rb
+++ mipt/db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+students = []
+
+File.foreach("MiPT_student_roster.csv") do |line|
+	student_row = line.split(",")
+	students << student_row
+end
+students.delete_at(0)
+
+students.each do |row|
+	#if teacher doesn't already exist, create new teacher given the 'assigned_teacher' column in student data
+	if Teacher.find_by(full_name: row[6])
+	else
+		Teacher.create(full_name: row[6]) 
+	end
+end
+
+students.each do |row|
+	#if student already exists, do nothing
+	if Student.find_by(first_name: student[0], last_name: student[1])
+	#if doesn't exist, create student with corre
+	else
+		teacher = Teacher.find_by(full_name: row[6])
+		Student.create(
+			first_name: row[0],
+			last_name: row[1],
+			grade_level: row[2],
+			gender: row[3],
+			gpa: row[4],
+			number_of_detentions: row[5],
+			teacher_id: teacher.id,
+			shirt_size: row[7])
+	end
+end
