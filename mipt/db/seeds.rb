11b08eedbb84a8c4c3daaeb569708479c40144dc
hypatiah <danielle.hassid@gmail.com>
2016-08-25 10:51:07 -0700

    Modify seed file attributes to integers and floats

--- mipt/db/seeds.rb
+++ mipt/db/seeds.rb
@@ -4,13 +4,14 @@ File.foreach("MiPT_student_roster.csv") do |line|
 	student_row = line.split(",")
 	students << student_row
 end
+
 students.delete_at(0)
 
 students.each do |row|
 	#if teacher doesn't already exist, create new teacher given the 'assigned_teacher' column in student data
 	if Teacher.find_by(full_name: row[6])
 	else
-		Teacher.create(full_name: row[6]) 
+		Teacher.create(full_name: row[6])
 	end
 end
 
@@ -23,10 +24,10 @@ students.each do |row|
 		Student.create(
 			first_name: row[0],
 			last_name: row[1],
-			grade_level: row[2],
+			grade_level: row[2].to_i,
 			gender: row[3],
-			gpa: row[4],
-			number_of_detentions: row[5],
+			gpa: row[4].to_f,
+			number_of_detentions: row[5].to_i,
 			teacher_id: teacher.id,
 			shirt_size: row[7])
 	end
