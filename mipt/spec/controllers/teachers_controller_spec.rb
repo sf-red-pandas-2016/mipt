0929733ca16d453fc3bf92e4edb61c04001a8ccc
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-08-26 15:12:44 -0700

    Update teachers_controller rspec

--- mipt/spec/controllers/teachers_controller_spec.rb
+++ mipt/spec/controllers/teachers_controller_spec.rb
@@ -2,38 +2,40 @@ require 'rails_helper'
 
 RSpec.describe TeachersController, :type => :controller do
 
+  let!(:teacher) { Teacher.create!(full_name: "Suzan Nosferatu") }
+
   describe "GET #index" do
     it "responds with status code 200" do
       get :index
       response.code.should eq("200")
     end
 
-    # it "renders the :index template" do
-    #   get :index
-    #   expect(response).to render_template("index")
-    # end
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template(:index)
+    end
 
   end
 
 
   describe "GET #show" do
 
-    # it "responds with status code 200" do
-    #   get :show, { id: game.id }
-    #   expect(response).to have_http_status 200
-    # end
+#TODO why is this failing?
+    it "responds with status code 200" do
+      get :teacher, { id: teacher.id }
+      expect(response).to have_http_status 200
+    end
 
-    # it "assigns the correct game as @game" do
-    #   get :show, { id: game.id }
-    #   expect(assigns(:game)).to eq(game)
-    # end
+    it "assigns the correct teacher as @teacher" do
+      get :show, { id: teacher.id}
+      expect(assigns(:teacher)).to eq(teacher)
+    end
 
-    # it "renders the :show template" do
-    #   get :show, { id: game.id }
-    #   expect(response).to render_template(:show)
-    # end
+    it "renders the :show template" do
+      get :show, { id: teacher.id }
+      expect(response).to render_template(:show)
+    end
 
   end
-
 
 end
