6e4524578fd6d1f324847aee5da40c4726812010
nelson chen <email": null>
2016-08-26 12:05:10 -0700

    Create new course

--- mipt/app/controllers/courses_controller.rb
+++ mipt/app/controllers/courses_controller.rb
@@ -5,12 +5,13 @@ class CoursesController < ApplicationController
 
   def new
     @course = Course.new
+    @teacher = Teacher.find(params[:teacher_id])
   end
 
   def create
   	@course = Course.new(course_params)
 
-  	if @course.save
+    if @course.save
   		redirect_to @course
   	else
   		render 'new'
@@ -19,6 +20,6 @@ class CoursesController < ApplicationController
 
    private
     def course_params
-      params.require(:course).permit(:title, :description)
+      params.require(:course).permit(:title, :description, :teacher_id)
     end
 end
